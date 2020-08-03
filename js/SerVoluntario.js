8357a4ca3644c812afc60db70e3e18019e729a9e
Alejandro Cipolatti <alejandro.cipolatti@hotmail.com>
2020-08-02 23:21:13 -0300

    Add instituciones, su punto en el map. Carousel

--- js/SerVoluntario.js
+++ js/SerVoluntario.js
@@ -6,7 +6,35 @@ $('#kids').on({
 
 $('#inst1').on({
     'click': function(){     
-        var place_id = "ChIJ93qT7VZENZQRHcP9QR5tvEo";   
+        var place_id = "ChIJN8oHULVFNZQRMBD_d98IFM0";   
+        $('#map').attr('src','https://www.google.com/maps/embed/v1/place?key=AIzaSyCJZyxRxT8rvAdqcR7Pj5t1HY80uX04fWQ&q=place_id:' + place_id);
+    }
+});
+
+$('#inst2').on({
+    'click': function(){     
+        var place_id = "ChIJU6_avtBFNZQRAnkYlnoHErI";   
+        $('#map').attr('src','https://www.google.com/maps/embed/v1/place?key=AIzaSyCJZyxRxT8rvAdqcR7Pj5t1HY80uX04fWQ&q=place_id:' + place_id);
+    }
+});
+
+$('#inst3').on({
+    'click': function(){     
+        var place_id = "ChIJN8oHULVFNZQRfI3hBty_QrY";   
+        $('#map').attr('src','https://www.google.com/maps/embed/v1/place?key=AIzaSyCJZyxRxT8rvAdqcR7Pj5t1HY80uX04fWQ&q=place_id:' + place_id);
+    }
+});
+
+$('#inst4').on({
+    'click': function(){     
+        var place_id = "ChIJN8oHULVFNZQRsJ0qiEtXpv0";   
+        $('#map').attr('src','https://www.google.com/maps/embed/v1/place?key=AIzaSyCJZyxRxT8rvAdqcR7Pj5t1HY80uX04fWQ&q=place_id:' + place_id);
+    }
+});
+
+$('#inst5').on({
+    'click': function(){     
+        var place_id = "ChIJe6oLBxtFNZQRYb-oJvTuzQo";   
         $('#map').attr('src','https://www.google.com/maps/embed/v1/place?key=AIzaSyCJZyxRxT8rvAdqcR7Pj5t1HY80uX04fWQ&q=place_id:' + place_id);
     }
 });
